a2f780df6c1b250533e73b84651f5ad73d1d1edd
Jeff McCune <jeff@puppetlabs.com>
2012-02-20 03:37:36 +0000

    (maint) Fix whitespace issues in agent.pp
    This patch fixes whitespace issues in the agent.pp file.  There are a
    number of tab characters following spaces and otherwise used as
    whitespace.  This patch moves the file to two space soft tab
    indentation.

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,38 +1,33 @@
 class zabbix::agent inherits zabbix {
-	$zabbix_userparameter_config_dir = "/etc/zabbix/zabbix_agentd"
-    $zabbix_agentd_conf = "$zabbix_config_dir/zabbix_agentd.conf"
+  $zabbix_userparameter_config_dir = "/etc/zabbix/zabbix_agentd"
+  $zabbix_agentd_conf = "$zabbix_config_dir/zabbix_agentd.conf"
 
-    package {
-        "zabbix-agent":
-            ensure	=> installed,
-			require	=> Yumrepo['itsc']
-    }
+  package { "zabbix-agent":
+    ensure => installed,
+    require => Yumrepo['itsc']
+  }
 
-    file {
-        $zabbix_userparameter_config_dir:
-            ensure 	=> directory,
-            owner 	=> root,
-            group 	=> root,
-            mode 	=> 755,
-            require => [ Package["zabbix-agent"], File["$zabbix_config_dir"] ];
-
-        $zabbix_agentd_conf:
-            owner 	=> root,
-            group 	=> root,
-            mode 	=> 644,
-            content => template("zabbix/zabbix_agentd_conf.erb"),
-			notify	=> Service['zabbix_agentd'],
-            require => [ Package["zabbix-agent"], File["$zabbix_config_dir"] ];
-	}
-
-    service {
-        "zabbix_agentd":
-            enable 		=> true,
-            ensure 		=> running,
-			hasstatus	=> false,
-			hasrestart	=> true,
-            require 	=> [ Package["zabbix-agent"], File["$zabbix_config_dir"], File["$zabbix_log_dir"], File["$zabbix_pid_dir"] ];
-    }
-	
+  file {
+    $zabbix_userparameter_config_dir:
+      ensure => directory,
+      owner => root,
+      group => root,
+      mode => 755,
+      require => [ Package["zabbix-agent"], File["$zabbix_config_dir"] ];
+    $zabbix_agentd_conf:
+      owner => root,
+      group => root,
+      mode => 644,
+      content => template("zabbix/zabbix_agentd_conf.erb"),
+      notify => Service['zabbix_agentd'],
+      require => [ Package["zabbix-agent"], File["$zabbix_config_dir"] ];
+  }
+  service { "zabbix_agentd":
+    enable => true,
+    ensure => running,
+    hasstatus => false,
+    hasrestart => true,
+    require => [ Package["zabbix-agent"], File["$zabbix_config_dir"], File["$zabbix_log_dir"], File["$zabbix_pid_dir"] ];
+  }
 }
 
