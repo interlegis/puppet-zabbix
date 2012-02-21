04a946d69bde5170dfebfbc5d87efcf98755391b
Jeff McCune <jeff@puppetlabs.com>
2012-02-21 02:52:39 +0000

    Bounce zabbix-agent when doing an initial install
    The zabix::agent class was getting the agent into a state where Puppet
    thought it was not running and could not start the service because the
    process is in fact running.
    
    This problem came about because we're replacing the init script in the
    first puppet run and the path to the PID file changes.
    
    This patch fixes the problem by stopping the agent in an exec resource
    if we replace the init script.

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -14,6 +14,10 @@ class zabbix::agent inherits zabbix {
     debian  => true,
     default => false,
   }
+  $zabbix_service_hasrestart = $osfamily ? {
+    debian  => false,
+    default => true,
+  }
   $zabbix_init_content = $osfamily ? {
     debian  => template("${module_name}/debian/zabbix-agent"),
     default => undef,
@@ -44,7 +48,14 @@ class zabbix::agent inherits zabbix {
     owner   => 0,
     group   => 0,
     mode    => 0755,
-    notify  => Service["zabbix_agentd"],
+    require => Package["zabbix-agent"],
+    notify  => [ Service["zabbix_agentd"], Exec["bounce-agent"] ],
+  }
+  exec { "bounce-agent":
+    command     => "/bin/bash /etc/init.d/zabbix-agent stop",
+    refreshonly => true,
+    require     => Package["zabbix-agent"],
+    before      => Service["zabbix_agentd"],
   }
 
   service { "zabbix_agentd":
@@ -53,8 +64,7 @@ class zabbix::agent inherits zabbix {
     name       => $zabbix_service_name,
     hasstatus  => $zabbix_service_hasstatus,
     status     => $zabbix_service_status,
-    hasrestart => true,
+    hasrestart => $zabbix_service_hasrestart,
     require    => [ Package["zabbix-agent"], File["$zabbix_config_dir"], File["$zabbix_log_dir"], File["$zabbix_pid_dir"] ];
   }
 }
-
