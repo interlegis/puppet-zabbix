6a0e19469ea411c4d606779cd2eeef326c16566f
Corey Frang <gnarf@gnarf.net>
2012-04-21 10:25:49 -0500

    Lets see if this works...

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,7 +1,8 @@
 class zabbix::agent (
   $zabbix_userparameter_config_dir = "/etc/zabbix/zabbix_agentd",
   $zabbix_agentd_conf              = "$zabbix_config_dir/zabbix_agentd.conf",
-  $zabbix_proxy                    = false
+  $zabbix_proxy                    = false,
+  $zabbix_server
 ) inherits zabbix {
   file {
     $zabbix_config_dir:
