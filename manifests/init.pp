f3cd68b02dd6853d8dffce8efe78f30d29d284dd
Corey Frang <gnarf@gnarf.net>
2012-04-21 10:27:56 -0500

    Variables in the right place

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,7 @@ class zabbix(
   $zabbix_config_dir = "/etc/zabbix",
   $zabbix_user_home_dir = "/var/lib/zabbix",
   $zabbix_log_dir = "/var/log/zabbix/",
-  $zabbix_pid_dir= "/var/run/zabbix/",
-  $zabbix_server
+  $zabbix_pid_dir= "/var/run/zabbix/"
 ) {
 
   user { 'zabbix':
