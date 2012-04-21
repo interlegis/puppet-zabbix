d9a1cbe6e6af8ccc41456f4f17737062266efe25
Corey Frang <gnarf@gnarf.net>
2012-04-21 10:35:57 -0500

    Some manifest tweaks

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,9 +1,12 @@
 class zabbix::agent (
-  $zabbix_userparameter_config_dir = "/etc/zabbix/zabbix_agentd",
-  $zabbix_agentd_conf              = "$zabbix_config_dir/zabbix_agentd.conf",
-  $zabbix_proxy                    = false,
+  $zabbix_proxy                    = undef,
   $zabbix_server
 ) inherits zabbix {
+  include zabbix::params
+  
+  $zabbix_userparameter_config_dir = ${zabbix::params::zabbix_userparameter_config_dir}
+  $zabbix_agentd_conf              = ${zabbix::params::zabbix_agentd_conf}
+  
   file {
     $zabbix_config_dir:
       ensure  => directory,
