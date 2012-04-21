898f3b24f387ae901d65928d536b8be8427ab336
Corey Frang <gnarf@gnarf.net>
2012-04-21 10:11:28 -0500

    trailing , not allowed here

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,6 +1,6 @@
 class zabbix::agent inherits zabbix (
   $zabbix_userparameter_config_dir = "/etc/zabbix/zabbix_agentd",
-  $zabbix_agentd_conf              = "$zabbix_config_dir/zabbix_agentd.conf",
+  $zabbix_agentd_conf              = "$zabbix_config_dir/zabbix_agentd.conf"
 ) {
   file {
     $zabbix_config_dir:
