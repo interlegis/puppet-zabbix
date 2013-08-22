0c5eaae1f08f15a793e4f05b38b209caf005c756
Fabio Rauber <fabiorauber@gmail.com>
2013-08-22 07:46:42 -0300

    Changed default user parameters folder and hostname

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,6 @@
 class zabbix::params {
 
-	$zabbix_userparameter_config_dir = "/etc/zabbix/zabbix_agentd"
+    $zabbix_userparameter_config_dir = "/etc/zabbix/zabbix_agentd.conf.d"
     $zabbix_agentd_conf = "$zabbix_config_dir/zabbix_agentd.conf"
 
 }
